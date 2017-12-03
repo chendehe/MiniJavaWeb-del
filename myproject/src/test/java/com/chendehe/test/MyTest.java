ce695906819bf2c23ab6fd23e6587e0c19bb89bd
shark <chendehe22@163.com>
2017-12-03 16:31:53 +0800

    test modules

---
+++ myproject/src/test/java/com/chendehe/test/MyTest.java
@@ -0,0 +1,17 @@
+package com.chendehe.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTest {
+
+    @Test
+    public void exampleTest() {
+        assertThat(2);
+    }
+}
