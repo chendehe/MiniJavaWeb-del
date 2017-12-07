33ff968ac49027d6c67f1fdebc292bd486033fbf
陈德鹤 <dehe.chen@baifendian.com>
2017-12-07 19:43:09 +0800

    checketyle

--- demo/src/main/java/com/chendehe/exception/ExceptionUtils.java
+++ demo/src/main/java/com/chendehe/exception/ExceptionUtils.java
@@ -4,6 +4,10 @@ import com.chendehe.config.Message;
 
 public class ExceptionUtils {
 
+  /**
+   * 异常处理工具.
+   * @param errorCode 错误码
+   */
   public static void exception(String errorCode) {
     try {
       throw new ValidationException(Message.message(errorCode));
