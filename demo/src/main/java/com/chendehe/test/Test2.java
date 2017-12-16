cef166445d03beccb38f1771853c554b887b69fa
shark <chendehe22@163.com>
2017-12-16 15:55:30 +0800

    add test classes

---
+++ demo/src/main/java/com/chendehe/test/Test2.java
@@ -0,0 +1,7 @@
+package com.chendehe.test;
+
+public class Test2 {
+  public static void main(String[] args) {
+    System.out.println((long)(0.5 * 60000000000L));
+  }
+}
--- demo/src/main/java/com/chendehe/util/RedisUtil.java
+++ demo/src/main/java/com/chendehe/util/RedisUtil.java
@@ -1,8 +1,6 @@
 package com.chendehe.util;
 
 import com.chendehe.config.RedisConfig;
-import io.codis.jodis.JedisResourcePool;
-import io.codis.jodis.RoundRobinJedisPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
@@ -30,15 +28,5 @@ public final class RedisUtil {
   public static String get(String key) {
     return jedis.get(key);
   }
-
-//  public static void main(String[] args) {
-//    JedisResourcePool jedisPool = RoundRobinJedisPool.create()
-//        .curatorClient("zkserver:2181", 30000).zkProxyDir("/jodis/xxx").build();
-//    try (Jedis jedis = jedisPool.getResource()) {
-//      jedis.set("foo", "bar");
-//      String value = jedis.get("foo");
-//      System.out.println(value);
-//    }
-//  }
 
 }
