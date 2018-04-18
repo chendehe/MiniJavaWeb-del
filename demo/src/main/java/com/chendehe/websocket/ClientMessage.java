067e2aaac571b2c40591f7066bdcd0b00edb6e13
shark <chendehe22@163.com>
2018-04-18 21:22:38 +0800

    websocket test

---
+++ demo/src/main/java/com/chendehe/websocket/ClientMessage.java
@@ -0,0 +1,21 @@
+package com.chendehe.websocket;
+
+/**
+ * 
+ * @ClassName: ClientMessage
+ * @Description: 客户端发送消息实体
+ * @author cheng
+ * @date 2017年9月27日 下午4:24:11
+ */
+public class ClientMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
