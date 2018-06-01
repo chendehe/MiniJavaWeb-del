30e68516c086fc0d4620c1e979693a1d9d2a7eab
shark <chendehe22@163.com>
2018-06-01 22:22:05 +0800

    配置Feign打印请求详细日志

--- feign-server/src/main/java/com/chendehe/cloud/feign/service/FeignLogConfig.java
+++ feign-server/src/main/java/com/chendehe/cloud/feign/service/FeignLogConfig.java
@@ -4,6 +4,9 @@ import feign.Logger.Level;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Feign 配置类，配置日志级别为FULL
+ */
 @Configuration
 public class FeignLogConfig {
 
