9425d023e24229f5bfab7389c63c81e4089455e6
陈德鹤 <dehe.chen@baifendian.com>
2018-06-02 16:24:26 +0800

    加注释

--- zuul-server/src/main/java/com/chendehe/cloud/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/com/chendehe/cloud/zuul/ZuulApplication.java
@@ -2,11 +2,14 @@ package com.chendehe.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * 网关，访问方式
+ *  1、http://localhost:1030/service-feign/111
+ *  2、http://localhost:1030/api-b/111
+ */
 @EnableZuulProxy
-@EnableEurekaClient
 @SpringBootApplication
 public class ZuulApplication {
 
