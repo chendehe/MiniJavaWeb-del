ba4dcc1554fb76d962c8f495f56d735c6923cf23
陈德鹤 <dehe.chen@baifendian.com>
2018-06-02 16:38:03 +0800

    禁用actuator鉴权

--- zuul-server/src/main/java/com/chendehe/cloud/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/com/chendehe/cloud/zuul/ZuulApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * 网关，访问方式
  *  1、http://localhost:1030/service-feign/111
  *  2、http://localhost:1030/api-b/111
+ *  查询路由映射 http://localhost:1030/routes
+ *  查询过滤器 http://localhost:1030/filters
  */
 @EnableZuulProxy
 @SpringBootApplication
