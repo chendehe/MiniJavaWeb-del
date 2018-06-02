fe721db4989c90d0aa8ec5f761fc52b169353344
陈德鹤 <dehe.chen@baifendian.com>
2018-06-02 15:41:19 +0800

    eureka修改为单节点方便测试

--- hystrix-turbine/src/main/java/com/chendehe/cloud/turbine/TurbineApplication.java
+++ hystrix-turbine/src/main/java/com/chendehe/cloud/turbine/TurbineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.ComponentScan;
 
 //聚合 HystrixDashboard 数据
+//此外，可以结合MQ来使用：Hystrix的监控数据发至MQ，Turbine消费MQ的数据
 @EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
