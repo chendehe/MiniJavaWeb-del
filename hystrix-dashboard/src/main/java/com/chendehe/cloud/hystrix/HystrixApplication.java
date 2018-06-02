54b79ce17e29ba378c0a1fdcccb3305a1b0b53e2
陈德鹤 <dehe.chen@baifendian.com>
2018-06-02 14:33:44 +0800

    添加hystrix-dashboard工程

---
+++ hystrix-dashboard/src/main/java/com/chendehe/cloud/hystrix/HystrixApplication.java
@@ -0,0 +1,19 @@
+package com.chendehe.cloud.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
+
+//可以用 hystrix-dashboard 可视化监控
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan("com.chendehe.cloud.hystrix")
+public class HystrixApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HystrixApplication.class, args);
+  }
+}
