3e6b25635779c22b6dbc7a31d9ea9d453e001e84
shark <chendehe22@163.com>
2018-06-03 14:30:21 +0800

    支持Kafka的Bus

--- config-server/src/main/java/com/chendehe/cloud/config/ConfigApplication.java
+++ config-server/src/main/java/com/chendehe/cloud/config/ConfigApplication.java
@@ -4,6 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * ****************RabbitMQ*******************
+ * 1、管理员身份安装otp_win64_20.3.exe
+ * 2、安装rabbitmq-server-3.7.5.exe
+ * 3、启动管理界面rabbitmq-plugins.bat enable rabbitmq_management
+ * 4、访问http://localhost:15672/，帐号guest：guest
+ * ****************Kafka*******************
+ * 1、安装ZooKeeper
+ * 2、安装Kafka
+ * 3、分别默认配置(此时配置服务无需任何配置)启动ZooKeeper和Kafka
+ * ****************************************
+ * > 配置更新后使用http://localhost:1050/bus/refresh刷新
+ * > 使用git wehhook自动更新！
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
