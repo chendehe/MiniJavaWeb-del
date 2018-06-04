8f48362adb1d02a28a9fd73eab12a5b3270520e3
陈德鹤 <dehe.chen@baifendian.com>
2018-06-04 19:54:15 +0800

    添加sleuth链路监控

--- feign-server/src/main/java/com/chendehe/cloud/feign/controller/UserController.java
+++ feign-server/src/main/java/com/chendehe/cloud/feign/controller/UserController.java
@@ -5,6 +5,8 @@ import com.chendehe.cloud.feign.service.UserServiceImpl;
 import com.chendehe.cloud.feign.vo.UserVo;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
   @Autowired
   UserServiceImpl service;
 
   @GetMapping("/{id}")
   public String findOne(@PathVariable String id) {
-    System.out.println("id:" + id);
+    LOGGER.info("id:{}", id);
     return service.findOne(id);
   }
 
   @GetMapping("/list")
   public ResponseEntity findAll(Page page) {
-    System.out.println("id:" + page);
+    LOGGER.info("page:{}", page);
     Map<String, Object> map = new HashMap<>(2);
     map.put("currentPage", page.getCurrentPage());
     map.put("pageSize", page.getPageSize());
@@ -37,6 +41,7 @@ public class UserController {
 
   @PostMapping("/")
   public ResponseEntity save(@RequestBody UserVo userVo) {
+    LOGGER.info("save:{}", userVo);
     return new ResponseEntity<>(service.save(userVo), HttpStatus.CREATED);
   }
 }
