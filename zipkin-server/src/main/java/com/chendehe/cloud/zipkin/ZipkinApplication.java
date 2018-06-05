0418d9e81fa8f8c98f1d87b8b72e65c4e5e202ca
陈德鹤 <dehe.chen@baifendian.com>
2018-06-05 20:18:19 +0800

    整合zipkin和auto2

---
+++ zipkin-server/src/main/java/com/chendehe/cloud/zipkin/ZipkinApplication.java
@@ -0,0 +1,15 @@
+package com.chendehe.cloud.zipkin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+public class ZipkinApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZipkinApplication.class, args);
+  }
+}
