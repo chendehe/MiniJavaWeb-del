858c3004ec293549063d4cb9434054c02b0f0a16
陈德鹤 <dehe.chen@baifendian.com>
2018-06-05 16:41:48 +0800

    修复依赖错误监控包

--- order-service/src/main/java/com/chendehe/Application.java
+++ order-service/src/main/java/com/chendehe/Application.java
@@ -2,10 +2,8 @@ package com.chendehe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableCircuitBreaker
 @SpringBootApplication
 @ComponentScan("com.chendehe")
 public class Application {
